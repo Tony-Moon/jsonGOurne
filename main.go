148e28dc7b6c88cf850744007d3e2ca7e9f2f5cb
Tony-Moon <anthonyowenmoon@gmail.com>
2019-10-29 15:42:03 -0500

    Better not see that .json file

--- main.go
+++ main.go
@@ -1,9 +1,59 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
+	"encoding/json"
+	"io"
+	"log"
+	"os"
 )
 
 func main() {
-	fmt.Print("Hello json!")
+	users := []User{
+		{
+			Username: "TonyMoon",
+			Password: "TonysPass",
+			Email:    "tonymoon@gmail.com",
+		},
+		{
+			Username: "RosaCruz",
+			Password: "RosasPass",
+			Email:    "rosaacruz@gmail.com",
+		},
+		{
+			Username: "FlorLuna",
+			Password: "FlorsPass",
+			Email:    "florluna@gmail.com",
+		},
+	}
+
+	db := Userdb{
+		Users: users,
+		Type:  "simple",
+	}
+
+	var buf = new(bytes.Buffer)
+	enc := json.NewEncoder(buf)
+	enc.Encode(db)
+
+	f, err := os.Create("user.db.json")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer f.Close()
+
+	io.Copy(os.Stdout, buf)
+}
+
+// User is a structure for the json files
+type User struct {
+	Username string
+	Password string
+	Email    string
+}
+
+// Userdb is a database of users
+type Userdb struct {
+	Users []User
+	Type  string
 }
