f3e9eb390d4007d77747b79f48e2f01e15e362a7
Tony-Moon <anthonyowenmoon@gmail.com>
2019-10-29 16:08:15 -0500

    thrid commit

--- main.go
+++ main.go
@@ -3,57 +3,55 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"os"
 )
 
 func main() {
-	users := []User{
-		{
-			Username: "TonyMoon",
-			Password: "TonysPass",
-			Email:    "tonymoon@gmail.com",
-		},
-		{
-			Username: "RosaCruz",
-			Password: "RosasPass",
-			Email:    "rosaacruz@gmail.com",
-		},
-		{
-			Username: "FlorLuna",
-			Password: "FlorsPass",
-			Email:    "florluna@gmail.com",
-		},
-	}
-
-	db := Userdb{
-		Users: users,
-		Type:  "simple",
-	}
-
-	var buf = new(bytes.Buffer)
-	enc := json.NewEncoder(buf)
-	enc.Encode(db)
-
-	f, err := os.Create("user.db.json")
+	f, err := os.Open("user.db.json")
 	if err != nil {
 		log.Fatalln(err)
 	}
 	defer f.Close()
 
-	io.Copy(os.Stdout, buf)
+	dec := json.NewDecoder(f)
+	db := Userdb{}
+	dec.Decode(&db)
+
+	fmt.Println(db)
+	// createJSONFile(db)
 }
 
 // User is a structure for the json files
 type User struct {
-	Username string
-	Password string
-	Email    string
+	Username string `json:"username,omitempty"`
+	Password string `json:"password,omitempty"`
+	Email    string `json:"email,omitempty"`
 }
 
 // Userdb is a database of users
 type Userdb struct {
-	Users []User
-	Type  string
+	Users []User `json:"users,omitempty"`
+	Type  string `json:"type,omitempty"`
+}
+
+func createJSONFile(db Userdb) {
+	users := []User{
+		{Username: "TonyMoon", Password: "TonysPass", Email: "tonymoon@gmail.com"},
+		{Username: "RosaCruz", Password: "RosasPass", Email: "rosaacruz@gmail.com"},
+		{Username: "FlorLuna", Password: "FlorsPass", Email: "florluna@gmail.com"},
+	}
+	db = Userdb{Users: users, Type: "Simple"}
+
+	var buf = new(bytes.Buffer)
+	enc := json.NewEncoder(buf)
+	enc.Encode(db)
+	f, err := os.Create("user.db.json")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer f.Close()
+	io.Copy(f, buf)
 }
